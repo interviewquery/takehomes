61f065ee8bef78fa3b00afe294a7063e910e32f5
Xue64 <rufelleemmanuelpactol7@gmail.com>
2023-08-28 16:20:14 +0800

    Modified to be in-line with formatting and standards

--- ncr_1/app.py
+++ ncr_1/app.py
@@ -8,34 +8,30 @@ from flask import Flask, render_template, request, g
 from flask_ngrok import run_with_ngrok
 
 
-def get_ngrok_token_from_yml():
-    with open('ngrok.yml', 'r') as yml_file:
-        config = yaml.safe_load(yml_file)
-        return config.get('authtoken', None)
+# makes sure that the tokens run properly
+def token_run():
+    def get_ngrok_token_from_yml():
+        with open('ngrok.yml', 'r') as yml_file:
+            config = yaml.safe_load(yml_file)
+            return config.get('authtoken', None)
 
+    ngrok_token = get_ngrok_token_from_yml()
+    if ngrok_token:
+        os.environ['NGROK_AUTH_TOKEN'] = ngrok_token
 
-ngrok_token = get_ngrok_token_from_yml()
-if ngrok_token:
-    os.environ['NGROK_AUTH_TOKEN'] = ngrok_token
+    def set_ngrok_token(token):
+        command = f"./ngrok authtoken {token}"
+        result = subprocess.run(command.split(), capture_output=True, text=True)
+        if result.returncode == 0:
+            print("Token set successfully!")
+        else:
+            print("Failed to set token. Error:", result.stderr)
 
-
-def set_ngrok_token(token):
-    command = f"./ngrok authtoken {token}"
-    result = subprocess.run(command.split(), capture_output=True, text=True)
-    if result.returncode == 0:
-        print("Token set successfully!")
+    token = get_ngrok_token_from_yml()
+    if token:
+        set_ngrok_token(token)
     else:
-        print("Failed to set token. Error:", result.stderr)
-
-
-token = get_ngrok_token_from_yml()
-if token:
-    set_ngrok_token(token)
-else:
-    print("Token not found in ngrok.yml!")
-
-app = Flask(__name__)
-run_with_ngrok(app)  # This line is added to make Flask work with ngrok
+        print("Token not found in ngrok.yml!")
 
 
 def get_db():
@@ -50,6 +46,21 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+
+def main():
+    token_run()
+    run_with_ngrok(app)  # This line is added to make Flask work with ngrok
+    app.run()
+
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    main()
+
+
+# ROUTES
+# Define your endpoints in this section
 
 app.teardown_appcontext(close_db)  # This ensures the database is closed after each request
 
@@ -92,6 +103,3 @@ def order_now():
     except sqlite3.Error as e:
         return f"<h1>Error submitting order: {str(e)}</h1>"
 
-
-if __name__ == "__main__":
-    app.run()
