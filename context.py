62f34960e5a2a78348fffe56a973cfff1673be52
Xue64 <rufelleemmanuelpactol7@gmail.com>
2023-09-13 22:13:33 +0800

    Revert

--- context.py
+++
@@ -1,5 +0,0 @@
-CURRENT_DIR = ''
-def generate_path(path:str):
-    return CURRENT_DIR + path
-
-DATASET_PATH = 'dataset/sales.csv'
--- driver.py
+++
@@ -1,5 +0,0 @@
-import iq_init
-
-
-def query(sql_query: str):
-    return iq_init.__linker__(sql_query)
